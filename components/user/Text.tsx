bbb90f62a4ad792f7c4f639380ea48f3ccb3b29a
f3ve <free.eve@pm.me>
2024-08-16 15:55:24 -0800

    Implemented text editing

--- components/user/Text.tsx
+++ components/user/Text.tsx
@@ -1,13 +1,45 @@
 'use client';
-import { useEditorDragAndDrop } from '@/hooks/useEditorDragAndDrop';
-import React from 'react';
+import { useNode } from '@craftjs/core';
+import React, { useEffect, useState } from 'react';
+import ContentEditable, { type ContentEditableEvent } from 'react-contenteditable';
 
-export const Text = ({ text, fontSize = '16' }: { text: string; fontSize?: string }) => {
-  const editorDragAndDrop = useEditorDragAndDrop();
+export type TextProps = {
+  text: string;
+  fontSize?: string;
+};
+
+export const Text = ({ text, fontSize = '16' }: TextProps) => {
+  const {
+    connectors: { connect, drag },
+    actions: { setProp },
+    hasSelectedNode,
+  } = useNode((state) => ({
+    hasSelectedNode: state.events.selected,
+  }));
+
+  const [editable, setEditable] = useState(false);
+
+  useEffect(() => {
+    !hasSelectedNode && setEditable(true);
+  }, [hasSelectedNode]);
+
+  function edit(e: ContentEditableEvent) {
+    setProp((props: TextProps) => (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, '')));
+  }
 
   return (
-    <div ref={editorDragAndDrop}>
-      <p style={{ fontSize }}>{text}</p>
+    <div
+      ref={(ref) => {
+        if (ref) connect(drag(ref));
+      }}
+    >
+      <ContentEditable
+        html={text}
+        disabled={!editable}
+        tagName="p"
+        style={{ fontSize: `${fontSize}px` }}
+        onChange={edit}
+      />
     </div>
   );
 };
