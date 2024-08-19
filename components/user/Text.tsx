189c5463fa923651223bb1ae7adf944f565ff232
f3ve <free.eve@pm.me>
2024-08-19 08:08:49 -0800

    feat: Can now change text font-size

--- components/user/Text.tsx
+++ components/user/Text.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useNode } from '@craftjs/core';
+import { FormControl, FormLabel, Slider, SliderOwnProps } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import ContentEditable, { type ContentEditableEvent } from 'react-contenteditable';
 
@@ -13,8 +14,10 @@ export const Text = ({ text, fontSize = '16' }: TextProps) => {
     connectors: { connect, drag },
     actions: { setProp },
     hasSelectedNode,
+    hasDraggedNode,
   } = useNode((state) => ({
     hasSelectedNode: state.events.selected,
+    hasDraggedNode: state.events.dragged,
   }));
 
   const [editable, setEditable] = useState(false);
@@ -26,6 +29,10 @@ export const Text = ({ text, fontSize = '16' }: TextProps) => {
   function edit(e: ContentEditableEvent) {
     setProp((props: TextProps) => (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, '')));
   }
+
+  const editFontSize: SliderOwnProps['onChange'] = (_, value) => {
+    setProp((props: TextProps) => (props.fontSize = value.toString()));
+  };
 
   return (
     <div
@@ -40,6 +47,19 @@ export const Text = ({ text, fontSize = '16' }: TextProps) => {
         style={{ fontSize: `${fontSize}px` }}
         onChange={edit}
       />
+      {hasSelectedNode && (
+        <FormControl className="text-additional-settings" size="small">
+          <FormLabel component="legend">Font Size</FormLabel>
+          <Slider
+            defaultValue={parseInt(fontSize)}
+            step={1}
+            min={7}
+            max={50}
+            valueLabelDisplay="auto"
+            onChange={editFontSize}
+          />
+        </FormControl>
+      )}
     </div>
   );
 };
