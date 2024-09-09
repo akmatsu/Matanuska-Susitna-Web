7e82cac1113edb7a7cfe43fe6ccb428ce5243f2d
f3ve <free.eve@pm.me>
2024-09-09 15:42:27 -0800

    fix: added className support in LinkCard component

--- sites/msb/components/LinkCard.tsx
+++ sites/msb/components/LinkCard.tsx
@@ -4,11 +4,12 @@ import { ReactNode } from 'react';
 export type LinkCardProps = {
   children: ReactNode;
   href: string;
+  className?: string;
 };
 
 export function LinkCard(props: LinkCardProps) {
   return (
-    <Link {...props} className="usa-card">
+    <Link {...props} className={`${props.className} usa-card`}>
       <div className="usa-card__container">{props.children}</div>
     </Link>
   );
