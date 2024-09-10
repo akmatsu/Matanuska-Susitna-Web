00cd93e8eb0d81e907c0bb9da2311cbc7023b439
f3ve <free.eve@pm.me>
2024-09-10 12:02:00 -0800

    setup postgresql database config

---
+++ sites/cms/appConfig.ts
@@ -0,0 +1,14 @@
+import { DatabaseProvider } from '@keystone-6/core/types';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const appConfig = {
+  database: {
+    provider: (process.env.DATABASE_PROVIDER as DatabaseProvider) ?? 'sqlite',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    name: process.env.DATABASE,
+  },
+};
---
+++ sites/cms/keystone.ts
@@ -0,0 +1,35 @@
+// Welcome to Keystone!
+//
+// This file is what Keystone uses as the entry-point to your headless backend
+//
+// Keystone imports the default export of this file, expecting a Keystone configuration object
+//   you can find out more at https://keystonejs.com/docs/apis/config
+
+import { config } from '@keystone-6/core';
+
+// to keep this file tidy, we define our schema in a different file
+import { lists } from './schema';
+
+// authentately here too, but you might move this elsewhere
+// when you write your list-level accication is configured separess control functions, as they typically rely on session data
+import { withAuth, session } from './auth';
+import { appConfig } from './appConfig';
+
+export default withAuth(
+  config({
+    db: {
+      // we're using sqlite for the fastest startup experience
+      //   for more information on what database might be appropriate for you
+      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
+      provider: appConfig.database.provider,
+      url: `${appConfig.database.provider}://${appConfig.database.user}:${appConfig.database.password}@${appConfig.database.host}:${appConfig.database.port}/${appConfig.database.name}`,
+    },
+    server: {
+      cors: { origin: ['*'], credentials: true },
+      port: 3333,
+    },
+    lists,
+    session,
+    telemetry: false,
+  }),
+);
