e82cd750ff84ece33d35ba920fe0ad0e8db3f507
f3ve <free.eve@pm.me>
2024-09-23 13:26:35 -0800

    created custom logo

---
+++ sites/cms/admin/config.tsx
@@ -0,0 +1,25 @@
+function CustomLogo() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '8px',
+      }}
+    >
+      <img
+        src="https://images.matsu.gov/logo.png"
+        alt="The Matanuska-Susitna Borough Seal"
+        width="50"
+        height="50"
+      ></img>
+      <h3>MSB CMS</h3>
+    </div>
+  );
+}
+
+export const components = {
+  Logo: CustomLogo,
+};
