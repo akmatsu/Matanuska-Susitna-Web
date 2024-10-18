365e1147fff75fd686f4102226719fe33cb254b2
f3ve <free.eve@pm.me>
2024-10-18 09:59:30 -0800

    fixed sessions

--- sites/cms/keystone.ts
+++ sites/cms/keystone.ts
@@ -1,26 +1,18 @@
-// Welcome to Keystone!
-//
-// This file is what Keystone uses as the entry-point to your headless backend
-//
-// Keystone imports the default export of this file, expecting a Keystone configuration object
-//   you can find out more at https://keystonejs.com/docs/apis/config
-
+// Keystone config docs: https://keystonejs.com/docs/apis/config
 import { config } from '@keystone-6/core';
-
-// to keep this file tidy, we define our schema in a different file
 import { lists } from './schema';
-
-// authentately*  here too, but you might move this elsewhere
-// when you write your list-level accication is configured separess control functions, as they typically rely on session data
-import { withAuth, session } from './auth';
+import { TypeInfo } from '.keystone/types';
 import { appConfig } from './appConfig';
-import { passportMiddleware, setupAzureADClient } from './customAuth';
+import {
+  type Session,
+  passportMiddleware,
+  setupAzureADClient,
+  session,
+} from './auth';
 
-export default config({
+export default config<TypeInfo<Session>>({
+  // https://keystonejs.com/docs/config/config#db
   db: {
-    // we're using sqlite for the fastest startup experience
-    //   for more information on what database might be appropriate for you
-    //   see https://keystonejs.com/docs/guides/choosing-a-database#title
     provider: appConfig.database.provider,
     url: `${appConfig.database.protocol}://${appConfig.database.user}:${appConfig.database.password}@${appConfig.database.host}:${appConfig.database.port}/${appConfig.database.name}`,
     extendPrismaSchema(schema) {
@@ -31,6 +23,8 @@ export default config({
       );
     },
   },
+
+  // https://keystonejs.com/docs/config/config#server
   server: {
     cors: {
       origin: [appConfig.server.originHost],
@@ -44,8 +38,9 @@ export default config({
     port: appConfig.server.port,
   },
 
+  // https://keystonejs.com/docs/config/config#ui
   ui: {
-    async pageMiddleware({ wasAccessAllowed }) {
+    async pageMiddleware({ wasAccessAllowed, context }) {
       if (!wasAccessAllowed) {
         return {
           kind: 'redirect',
@@ -55,7 +50,15 @@ export default config({
     },
   },
 
+  // https://keystonejs.com/docs/config/lists
   lists,
+
+  // https://keystonejs.com/docs/config/session
   session,
+
+  // https://keystonejs.com/docs/reference/telemetry#how-to-opt-out
   telemetry: false,
+
+  // https://keystonejs.com/docs/guides/images-and-files
+  storage: {},
 });
