e897085f9b9fee0ea0518d710f45f6f0e62b1bf1
f3ve <free.eve@pm.me>
2024-10-30 15:58:04 -0800

    feat: added markdown view

--- sites/cms/components/RichEditor.tsx
+++ sites/cms/components/RichEditor.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { MdEditorProps } from '@msb/md-editor/dist/components/Editor/types';
+import { Button } from '@keystone-ui/button';
+import { TextArea } from '@keystone-ui/fields';
 import '@msb/md-editor/styles.css';
 
 const MdEditor = dynamic(
@@ -11,10 +13,28 @@ const MdEditor = dynamic(
 
 export function RichEditor(props: MdEditorProps) {
   const [isClient, setIsClient] = useState(false);
+  const [showCode, setShowCode] = useState(false);
   useEffect(() => setIsClient(true));
 
   if (isClient) {
-    return <MdEditor {...props} />;
+    return (
+      <div>
+        <Button onClick={() => setShowCode(!showCode)}>
+          {showCode ? 'Rich Editor' : 'View Markdown'}
+        </Button>
+        {showCode ? (
+          <TextArea
+            value={props.initialValue}
+            onChange={(e) => {
+              props.onChange?.(e.target.value);
+            }}
+            size="large"
+          />
+        ) : (
+          <MdEditor {...props} />
+        )}
+      </div>
+    );
   } else {
     return <div>Editor is loading...</div>;
   }
