49aa43cd680e3228d0144ba4685156b3c09419b2
f3ve <free.eve@pm.me>
2024-11-04 16:57:50 -0900

    implemented primary action button ui

--- sites/msb/components/MarkdownRenderer/components/PrimaryActionButton.tsx
+++ sites/msb/components/MarkdownRenderer/components/PrimaryActionButton.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@trussworks/react-uswds';
 
+// TODO! Make the button trigger the primary action
+
 export async function PrimaryActionButton({ label }: { label: string }) {
   return <Button type="button">{label}</Button>;
 }
