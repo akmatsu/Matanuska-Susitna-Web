3a6aa42896f9b35a8ede26b3da4a79ce70fa1724
f3ve <free.eve@pm.me>
2025-01-06 15:50:31 -0900

    created the widgets web app

---
+++ sites/widgets/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="max-w-6xl mx-auto flex justify-center items-center h-screen">
+      <h1 className="text-6xl">Matanuska-Susitna Web Widgets</h1>
+    </div>
+  );
+}
---
+++ sites/widgets/app/widgets/map/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Button } from '@matsugov/ui';
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
+}
