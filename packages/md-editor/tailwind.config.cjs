79f6a756162f25c3997ae7f6a549bc9ff1898243
Freedom Evenden <free.eve@pm.me>
2025-01-09 11:47:37 -0900

    fix: experimental fix for cms build issues

--- packages/md-editor/tailwind.config.cjs
+++ packages/md-editor/tailwind.config.cjs
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const msbConfig = require('@matsugov/tw-config');
 const { addDynamicIconSelectors } = require('@iconify/tailwind');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  extends: [msbConfig],
   important: true,
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
