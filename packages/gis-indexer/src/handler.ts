8f8dc0b466f3ae1dcc4ac7eb9802aadc37bdaef1
f3ve <free.eve@pm.me>
2025-01-16 08:24:20 -0900

    chore: created ESRI API Key

--- packages/gis-indexer/src/handler.ts
+++ packages/gis-indexer/src/handler.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import GISRequest from '@arcgis/core/request.js';
+import { request } from '@esri/arcgis-rest-request';
 
 export async function handler(): Promise<void> {
-  console.log(GISRequest);
+  
 }
