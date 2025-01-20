5d55a4e9f63860d16556fc9f7a7fb5fe0e9e1baf
f3ve <free.eve@pm.me>
2025-01-20 09:53:02 -0900

    wip: working on GIS indexer

--- packages/gis-indexer/src/handler.ts
+++ packages/gis-indexer/src/handler.ts
@@ -1,6 +1,23 @@
-import axios from 'axios';
-import { request } from '@esri/arcgis-rest-request';
+import 'dotenv/config';
+import { request, ApiKeyManager } from '@esri/arcgis-rest-request';
 
 export async function handler(): Promise<void> {
-  
+  const authentication = ApiKeyManager.fromKey(
+    process.env.ESRI_API_KEY as string,
+  );
+  const url =
+    'https://www.arcgis.com/sharing/rest/portals/19ccd04ffc8b432bbea44b6c727dc424';
+
+  const res = await request(`${url}/community/users`, {
+    authentication,
+    httpMethod: 'GET',
+    params: {
+      f: 'json',
+      q: 'freedom',
+    },
+  });
+
+  console.log(res);
 }
+
+handler();
