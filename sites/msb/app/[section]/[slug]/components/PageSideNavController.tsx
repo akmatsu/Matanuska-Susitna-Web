cd96950f53737e5f1cd8f46b4acaea660c0b1f77
f3ve <free.eve@pm.me>
2025-05-05 10:06:53 -0800

    wip: improving assembly pages

--- sites/msb/app/[section]/[slug]/components/PageSideNavController.tsx
+++ sites/msb/app/[section]/[slug]/components/PageSideNavController.tsx
@@ -9,22 +9,22 @@ export function PageSideNavController(props: {
   return (
     <div className="grid grid-cols-12 gap-8">
       {props.showSideNav && (
-        <div className="col-span-3">
+        <div className="col-span-3 hidden md:block">
           <InPageNavigation />
         </div>
       )}
       <div
-        className={clsx('flex flex-col gap-8', {
-          'col-span-6': props.showSideNav,
-          'col-span-8': !props.showSideNav,
+        className={clsx('flex flex-col gap-8 col-span-12', {
+          'md:col-span-6': props.showSideNav,
+          'md:col-span-8': !props.showSideNav,
         })}
       >
         {props.children}
       </div>
       <div
-        className={clsx({
-          'col-span-3': props.showSideNav,
-          'col-span-4': !props.showSideNav,
+        className={clsx('hidden md:block', {
+          'md:col-span-3': props.showSideNav,
+          'md:col-span-4': !props.showSideNav,
         })}
       >
         <aside className="flex flex-col gap-8">{props.rightSide}</aside>
--- sites/msb/app/[section]/[slug]/components/PageSidebar.tsx
+++ sites/msb/app/[section]/[slug]/components/PageSidebar.tsx
@@ -10,6 +10,9 @@ import { PageListItems } from './PageListItems';
 import { PageDistricts } from './PageDistricts';
 import { PageParentOrgUnit } from './PageParentOrgUnit';
 import { PageChildrenOrgUnits } from './PageChildrenOrgUnits';
+import { PageSection } from './PageSection';
+import { Card, CardBody, CardHeader, CardTitle } from '@matsugov/ui';
+import Link from 'next/link';
 
 export function PageSidebar({
   page,
@@ -25,6 +28,28 @@ export function PageSidebar({
       <PageMap {...config.map} itemId={page.title} />
 
       {page.primaryAction && <PageActions primaryAction={page.primaryAction} />}
+      {page.memberName && (
+        <PageSection title="Assembly Member">
+          <Card>
+            <CardHeader>
+              <div className="flex items-center justify-between gap-4">
+                <CardTitle>{page.memberName}</CardTitle>
+                {page.photo && (
+                  <img
+                    src={page.photo?.file.url}
+                    className="rounded-full size-20"
+                  />
+                )}
+              </div>
+            </CardHeader>
+            <CardBody>
+              <p>{page.bio}</p>
+              <Link href={`tel${page.phone}`}>{page.phone}</Link>
+              <Link href={`mailto:${page.email}`}>{page.email}</Link>
+            </CardBody>
+          </Card>
+        </PageSection>
+      )}
 
       <PageDocuments documents={page.documents} />
 
