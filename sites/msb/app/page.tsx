291448000d3f3393c93e8bcafa97da5c16db8238
f3ve <free.eve@pm.me>
2025-06-09 09:34:54 -0800

    chore: triggering status checks

--- sites/msb/app/page.tsx
+++ sites/msb/app/page.tsx
@@ -98,6 +98,7 @@ export default async function Home() {
           <SearchDynamicWrapper />
         </div>
       </Hero>
+
       <section className="flex justify-center items-center bg-base-lightest py-4 px-2">
         <div className=" max-w-[900px] w-full">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 w-full">
