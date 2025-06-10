dd206ff40c3b42616e38247bfe2c3adcf7f77c28
f3ve <free.eve@pm.me>
2025-06-10 10:32:38 -0800

    fix: internal links with no target no longer cause server errors

--- sites/msb/components/server/MarkdownRenderer/components/InternalLink.tsx
+++ sites/msb/components/server/MarkdownRenderer/components/InternalLink.tsx
@@ -7,8 +7,8 @@ import Link from 'next/link';
 import { plural } from 'pluralize';
 import v from 'voca';
 
-function getUrl(data: GetInternalLinkDataQuery, list: string) {
-  if (data.getInternalLink) {
+function getUrl(data: GetInternalLinkDataQuery, list?: string) {
+  if (data.getInternalLink && list) {
     if ('slug' in data.getInternalLink) {
       return `/${v.slugify(plural(list))}/${data.getInternalLink.slug}`;
     }
@@ -26,48 +26,50 @@ function getUrl(data: GetInternalLinkDataQuery, list: string) {
 }
 
 export async function InternalLink(props: {
-  list: string;
-  itemID: string;
+  list?: string;
+  itemID?: string;
   children: string;
 }) {
-  const { data } = await getClient().query({
-    query: GET_INTERNAL_LINK_DATA,
-    variables: {
-      id: props.itemID,
-      list: props.list,
-    },
-  });
+  if (props.list && props.itemID) {
+    const { data, errors, error } = await getClient().query({
+      query: GET_INTERNAL_LINK_DATA,
+      variables: {
+        id: props.itemID,
+        list: props.list,
+      },
+    });
 
-  const url = getUrl(data, props.list);
+    const url = getUrl(data, props.list);
 
-  const isExternalLink =
-    !!url?.startsWith('http://') || !!url?.startsWith('https://');
+    const isExternalLink =
+      !!url?.startsWith('http://') || !!url?.startsWith('https://');
 
-  if (url)
-    return (
-      <Link
-        href={url}
-        target={isExternalLink ? '_blank' : undefined}
-        referrerPolicy="no-referrer"
-        className={clsx({
-          'line-through !text-error': url === '/not-found',
-        })}
-      >
-        {props.children}{' '}
-        {isExternalLink && (
-          <span className="icon-[mdi--external-link] -mb-0.5" />
-        )}
-      </Link>
-    );
-  else
-    return (
-      <Tooltip
-        content="Oops! Looks like this link is broken. We are working on it!"
-        className="text-center"
-      >
-        <span className="text-error line-through cursor-not-allowed">
-          {props.children} <span className="icon-[mdi--error] -mb-0.5" />
-        </span>
-      </Tooltip>
-    );
+    if (url && !errors?.length && !error)
+      return (
+        <Link
+          href={url}
+          target={isExternalLink ? '_blank' : undefined}
+          referrerPolicy="no-referrer"
+          className={clsx({
+            'line-through !text-error': url === '/not-found',
+          })}
+        >
+          {props.children}{' '}
+          {isExternalLink && (
+            <span className="icon-[mdi--external-link] -mb-0.5" />
+          )}
+        </Link>
+      );
+  }
+
+  return (
+    <Tooltip
+      content="Oops! Looks like this link is broken. We are working on it!"
+      className="text-center"
+    >
+      <span className="!text-error !line-through !cursor-not-allowed">
+        {props.children} <span className="icon-[mdi--error] -mb-0.5" />
+      </span>
+    </Tooltip>
+  );
 }
