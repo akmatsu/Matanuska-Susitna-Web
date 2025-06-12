e9c6f10cbfa059fe9f877b1f2560cb614300fc6f
f3ve <free.eve@pm.me>
2025-06-12 10:44:39 -0800

    fix: fixed remark-directive parsing invalid directives. Also fixed <br /> tags appearing in body

--- sites/msb/global.d.ts
+++ sites/msb/global.d.ts
@@ -1,6 +1,7 @@
 import { ComponentProps } from 'react';
-import { Process, Step } from './components/server/MarkdownRenderer/components';
+import { Process } from './components/server/MarkdownRenderer/components/Process';
 import { InternalLink } from './components/server/MarkdownRenderer/components/InternalLink';
+import { Step } from './components/server/MarkdownRenderer/components/Step';
 
 declare module 'react' {
   namespace JSX {
