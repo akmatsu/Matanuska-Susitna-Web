80aa47945830aebebe728ab27f14ae261d2f4f68
f3ve <free.eve@pm.me>
2025-07-25 14:00:05 -0800

    feat: created the add to calendar functionality

--- packages/ui/src/components/DropdownButton.tsx
+++ packages/ui/src/components/DropdownButton.tsx
@@ -1,12 +1,13 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { Button } from './Button';
-import { ComponentProps, ReactNode } from 'react';
+import { Button, ButtonProps } from './Button';
+import React, { ReactNode } from 'react';
 
-export function DropdownButton(props: {
-  buttonProps?: ComponentProps<typeof Button>;
+export function DropdownButton<T extends React.ElementType = 'a'>(props: {
+  buttonProps?: ButtonProps;
   label?: ReactNode;
-  items?: ReactNode[];
+  items?: { label: string; href?: string; action?: () => void }[];
   hideIcon?: boolean;
+  linkAs?: T;
 }) {
   return (
     <Menu>
@@ -18,11 +19,29 @@ export function DropdownButton(props: {
       </Button>
       <MenuItems
         anchor="bottom"
-        className="rounded shadow bg-white w-(--button-width)"
+        className="rounded shadow bg-white min-w-(--button-width) p-2 flex flex-col"
       >
         {props.items?.length ? (
           props.items?.map((item, index) => (
-            <MenuItem key={index}>{item}</MenuItem>
+            <MenuItem key={index}>
+              {item.action ? (
+                <button
+                  onClick={item.action}
+                  className="w-full text-left after:icon-[mdi--download] after:ml-1 after:-mb-1 p-2 hover:cursor-pointer transition-colors data-focus:bg-blue-100 rounded"
+                >
+                  {item.label}
+                </button>
+              ) : (
+                <a
+                  href={item.href!}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-black p-2 w-full text-left no-underline after:icon-[mdi--external-link] after:ml-1 after:-mb-1 transition-colors  data-focus:bg-blue-100 rounded"
+                >
+                  {item.label}
+                </a>
+              )}
+            </MenuItem>
           ))
         ) : (
           <MenuItem>
