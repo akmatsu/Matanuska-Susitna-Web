79e528399e8b49185a1c33ee0278db5213608d47
f3ve <free.eve@pm.me>
2025-07-28 09:56:08 -0800

    feat: now grabbing all of the upcoming meetings

--- sites/msb/components/static/Meetings.tsx
+++ sites/msb/components/static/Meetings.tsx
@@ -1,26 +1,57 @@
 import { Button } from '@matsugov/ui/Button';
-import { calendar } from '@googleapis/calendar';
-import { MeetingCard } from './MeetingCard';
+import { calendar, calendar_v3 } from '@googleapis/calendar';
+import { Meeting, MeetingCard } from './MeetingCard';
 
 export async function Meetings() {
   const calApi = calendar('v3');
-  const res = await calApi.events.list({
-    key: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY,
-    calendarId: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_MAIN_ID,
-    timeMin: new Date().toISOString(),
-    singleEvents: true,
-    orderBy: 'startTime',
-    maxResults: 4,
-    q: 'planning commission',
-  });
+  function getCalendarParams(calendarId: string | undefined, query?: string) {
+    return {
+      key: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY,
+      calendarId: calendarId,
+      timeMin: new Date().toISOString(),
+      singleEvents: true,
+      orderBy: 'startTime',
+      maxResults: 4,
+      q: query,
+    } satisfies calendar_v3.Params$Resource$Events$List;
+  }
 
-  const items =
-    res.data.items?.map((item) => ({
+  const [resMain, resAssembly, resPlanning, resService] = await Promise.all([
+    calApi.events.list(
+      getCalendarParams(process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_MAIN_ID),
+    ),
+    calApi.events.list(
+      getCalendarParams(process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_ASSEMBLY_ID),
+    ),
+    calApi.events.list(
+      getCalendarParams(process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_PLANNING_ID),
+    ),
+    calApi.events.list(
+      getCalendarParams(process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_SERVICE_ID),
+    ),
+  ]);
+
+  function parseMeeting(item: calendar_v3.Schema$Event) {
+    return {
       id: item.id,
       date: item.start?.date || item.start?.dateTime,
       location: item.location || 'No location provided',
       title: item.summary || 'No title provided',
-    })) || [];
+    } satisfies Meeting;
+  }
+
+  const items = [
+    ...(resMain.data.items ? resMain.data.items.map(parseMeeting) : []),
+    ...(resAssembly.data.items ? resAssembly.data.items.map(parseMeeting) : []),
+    ...(resPlanning.data.items ? resPlanning.data.items.map(parseMeeting) : []),
+    ...(resService.data.items ? resService.data.items.map(parseMeeting) : []),
+  ]
+    .sort((a, b) => {
+      const dateA = new Date(a.date ?? '').getTime();
+      const dateB = new Date(b.date ?? '').getTime();
+      return dateA - dateB;
+    })
+    .splice(0, 4);
 
   return (
     <>
