fc2550db703b7e08efc45cfba2dac6d525b1d889
f3ve <free.eve@pm.me>
2025-08-05 12:57:22 -0800

    chore: triggering build

--- sites/msb/auth.ts
+++ sites/msb/auth.ts
@@ -3,6 +3,7 @@ import MicrosoftEntraId from 'next-auth/providers/microsoft-entra-id';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
+
   providers: [
     MicrosoftEntraId({
       authorization: {
