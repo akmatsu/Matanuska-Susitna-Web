b4977c22a10596805288c4ed5314ffaa4ad22b53
f3ve <free.eve@pm.me>
2025-08-04 16:11:27 -0800

    fix: set trustHost manually

--- sites/msb/auth.ts
+++ sites/msb/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import MicrosoftEntraId from 'next-auth/providers/microsoft-entra-id';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     MicrosoftEntraId({
       authorization: {
