4f0544793cd1367f93c278594814fc0af973a233
f3ve <free.eve@pm.me>
2025-09-08 13:38:27 -0800

    feat: changed colors of icons and added a border on featured icons card

--- sites/msb/components/static/Page/toolbeltHighlight.tsx
+++ sites/msb/components/static/Page/toolbeltHighlight.tsx
@@ -59,12 +59,10 @@ export function ToolbeltHighlight(props: {
   return (
     <Link
       href={url}
-      className="group w-28 rounded p-2 shadow-lg bg-white hover:bg-gray-100 text-base-darkest font-bold no-underline flex flex-col items-center gap-2 text-center  border-b-4 border-primary hover:border-primary-dark"
+      className="group w-28 rounded p-2 shadow-lg bg-white hover:bg-gray-100 text-base-darkest font-bold no-underline flex flex-col items-center gap-2 text-center border border-base-lighter hover:border-base-light  border-b-4 border-b-primary hover:border-b-primary-dark"
     >
-      <div className="size-14 bg-surface-primary rounded-full aspect-square flex justify-center items-center">
-        <span
-          className={`iconify size-8 ${item.icon} text-secondary transition-colors group-hover:text-secondary-dark`}
-        ></span>
+      <div className="size-14 bg-primary rounded-full aspect-square flex justify-center items-center group-hover:bg-primary-dark transition-colors">
+        <span className={`iconify size-8 ${item.icon} text-white`}></span>
       </div>
       <span className="text-sm">{item.linkedItem?.label}</span>
     </Link>
