e97a2815d1454ca865a41f267b12f2f4f7a78ff9
f3ve <free.eve@pm.me>
2025-09-15 16:49:53 -0800

    feat: updated the government page

---
+++ sites/msb/components/static/LinkIconCard.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+
+export function LinkIconCard(props: {
+  href: string;
+  icon?: string | null;
+  title?: string | null;
+  description?: string | null;
+  className?: string;
+}) {
+  return (
+    <Link
+      href={props.href}
+      className={clsx(
+        'flex flex-col md:flex-row gap-3 col-span-1 w-full text-black no-underline group rounded transition-colors overflow-hidden border border-base-lighter hover:border-base-light',
+        props.className,
+      )}
+    >
+      <div className="h-fit md:h-full bg-base-lightest p-4 w-full md:w-fit flex justify-center items-center">
+        <div className="aspect-square size-20 p-4 rounded-full bg-primary group-hover:bg-primary-dark justify-center items-center flex transition-colors">
+          <span className={clsx('size-full text-white', props.icon)} />
+        </div>
+      </div>
+      <div className="p-4">
+        <h3 className="text-xl font-semibold text-primary group-hover:text-primary-dark transitions-colors">
+          {props.title}
+        </h3>
+        <p>{props.description}</p>
+      </div>
+    </Link>
+  );
+}
