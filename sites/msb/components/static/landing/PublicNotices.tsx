25dd4d103f7ffb0872944140170039dafb9fc38e
f3ve <free.eve@pm.me>
2025-09-19 10:45:27 -0800

    feat: fixed public notice sorting

--- sites/msb/components/static/landing/PublicNotices.tsx
+++ sites/msb/components/static/landing/PublicNotices.tsx
@@ -5,6 +5,8 @@ import { FragmentType, getFragmentData, gql } from '@msb/js-sdk/gql';
 const publicNoticeListFragment = gql(`
   fragment PublicNoticeList on PublicNotice {
     id
+    urgency
+    publishAt
     ...PublicNoticeFields
   }
 `);
@@ -13,10 +15,23 @@ export function PublicNotices(props: {
   items: FragmentType<typeof publicNoticeListFragment>[];
 }) {
   const items = getFragmentData(publicNoticeListFragment, props.items);
+
+  if (!items?.length) return null;
+
+  const sorted = [...items].sort((a, b) => {
+    const aPriority = a.urgency ?? Infinity;
+    const bPriority = b.urgency ?? Infinity;
+    if (aPriority === bPriority) {
+      return new Date(b.publishAt).getTime() - new Date(a.publishAt).getTime();
+    }
+
+    return aPriority - bPriority;
+  });
+
   return (
     <div className="flex flex-col items-center gap-4">
       <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
-        {items.map((item, index) => (
+        {sorted.map((item, index) => (
           <PublicNoticeCard
             publicNotice={item}
             key={item.id}
