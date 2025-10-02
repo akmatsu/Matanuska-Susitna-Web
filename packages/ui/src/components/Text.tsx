fc5be91efe0ada4967c693f3defff54a9d8eb778
f3ve <free.eve@pm.me>
2025-10-02 15:29:41 -0800

    feat: added the officers section to the government page

---
+++ packages/ui/src/components/Text.tsx
@@ -0,0 +1,75 @@
+import clsx from 'clsx';
+import { JSX } from 'react';
+
+export type TextColor =
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'accent'
+  | 'dark'
+  | 'light';
+
+export type TextType =
+  | 'body'
+  | 'heading1'
+  | 'heading2'
+  | 'heading3'
+  | 'subtitle'
+  | 'label';
+
+export interface TextProps {
+  children?: React.ReactNode;
+  type?: TextType;
+  color?: TextColor;
+  className?: string;
+  noMargins?: boolean;
+}
+
+export function Text({
+  type = 'body',
+  color = 'default',
+  ...props
+}: TextProps) {
+  const Tag = getType(type);
+  return (
+    <Tag
+      className={clsx(
+        {
+          'text-white': color === 'light',
+          'text-black': color === 'dark',
+          'text-primary': color === 'primary',
+          'text-secondary': color === 'secondary',
+          'text-accent-warm': color === 'accent',
+          'text-4xl font-bold': type === 'heading1',
+          'text-2xl font-bold': type === 'heading2',
+          'text-xl font-semibold': type === 'heading3',
+          'mb-4':
+            !props.noMargins && (type === 'heading1' || type === 'heading2'),
+          'mb-3': !props.noMargins && type === 'heading3',
+        },
+        props.className,
+      )}
+    >
+      {props.children}
+    </Tag>
+  );
+}
+
+function getType(type: TextType): keyof JSX.IntrinsicElements {
+  switch (type) {
+    case 'body':
+      return 'p';
+    case 'heading1':
+      return 'h1';
+    case 'heading2':
+      return 'h2';
+    case 'heading3':
+      return 'h3';
+    case 'label':
+      return 'p';
+    case 'subtitle':
+      return 'h2';
+    default:
+      return 'p';
+  }
+}
--- sites/msb/app/departments/page.tsx
+++ sites/msb/app/departments/page.tsx
@@ -2,11 +2,11 @@ import { MarkdownRenderer } from '@/components/server/MarkdownRenderer';
 import { PageContainer } from '@/components/static/Page';
 import { ProseWrapper } from '@/components/static/ProseWrapper';
 import { gql } from '@msb/js-sdk/gql';
-import clsx from 'clsx';
-import Link from 'next/link';
+import { Text } from '@matsugov/ui/Text';
 import { notFound } from 'next/navigation';
 import { getClientHandler } from '@/utils/apollo/utils';
 import { LinkIconCard } from '@/components/static/LinkIconCard';
+import { DarkFlatCard } from '@/components/static/LandingPage/ProminentCard';
 
 const metaQuery = gql(`
   query GetDepartmentsPageMeta {
@@ -97,33 +97,23 @@ export default async function DepartmentsPage() {
       </PageContainer>
       <section className="bg-surface-primary text-white pb-8">
         <PageContainer size={pageSize} breakPoint="sm" hideBreadcrumbs>
-          <h2 className="text-2xl font-bold mb-4">Officers</h2>
+          <Text type="heading2">Officers</Text>
           <div className="flex gap-4 flex-wrap items-stretch justify-center">
             {offices.map((o) => (
-              <Link
-                href={`/departments/${o.slug}`}
+              <DarkFlatCard
                 key={o.id}
-                className="flex gap-3 max-w-xs text-white no-underline group hover:bg-white/3 p-2 rounded transition-colors"
-              >
-                <div>
-                  <div className="aspect-square size-20 p-4 rounded-full bg-primary justify-center items-center flex">
-                    <span className={clsx('size-full', o.icon)} />
-                  </div>
-                </div>
-                <div>
-                  <h3 className="text-xl font-semibold text-secondary">
-                    {o.title}
-                  </h3>
-                  <p className="text-white">{o.description}</p>
-                </div>
-              </Link>
+                title={o.title!}
+                icon={o.icon}
+                description={o.description}
+                href={`/departments/${o.slug}`}
+              />
             ))}
           </div>
         </PageContainer>
       </section>
       <section>
         <PageContainer size={pageSize} breakPoint="sm" hideBreadcrumbs>
-          <h2 className="text-2xl font-bold mb-4">Departments</h2>
+          <Text type="heading2">Departments</Text>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {departments.map((d) => (
               <LinkIconCard
