057d4b31e7cb7f92280f272657519980760ec7a3
f3ve <free.eve@pm.me>
2025-10-03 12:24:35 -0800

    fix: fixed build errors

--- packages/ui/src/components/Text.tsx
+++ packages/ui/src/components/Text.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { JSX } from 'react';
+import React, { JSX } from 'react';
 
 export type TextColor =
   | 'default'
@@ -29,35 +29,36 @@ export interface TextProps {
 export function Text({
   type = 'body',
   color = 'default',
-  ...props
+  children,
+  className,
+  noMargins,
+  ...rest
 }: TextProps) {
   const Tag = getType(type);
-  return (
-    <Tag
-      className={clsx(
-        {
-          'text-white': color === 'light',
-          'text-black': color === 'dark',
-          'text-primary': color === 'primary',
-          'text-secondary': color === 'secondary',
-          'text-accent-warm': color === 'accent',
-          'text-4xl font-bold': type === 'heading1',
-          'text-2xl font-bold': type === 'heading2',
-          'text-sm': type === 'body-sm',
-          'text-xl font-semibold': type === 'heading3',
-          'mb-4':
-            !props.noMargins && (type === 'heading1' || type === 'heading2'),
-          'mb-3': !props.noMargins && type === 'heading3',
-        },
-        props.className,
-      )}
-    >
-      {props.children}
-    </Tag>
+  const computedClassName = clsx(
+    {
+      'text-white': color === 'light',
+      'text-black': color === 'dark',
+      'text-primary': color === 'primary',
+      'text-secondary': color === 'secondary',
+      'text-accent-warm': color === 'accent',
+      'text-4xl font-bold': type === 'heading1',
+      'text-2xl font-bold': type === 'heading2',
+      'text-sm': type === 'body-sm',
+      'text-xl font-semibold': type === 'heading3',
+      'mb-4': !noMargins && (type === 'heading1' || type === 'heading2'),
+      'mb-3': !noMargins && type === 'heading3',
+    },
+    className,
+  );
+  return React.createElement(
+    Tag,
+    { className: computedClassName, ...rest },
+    children,
   );
 }
 
-function getType(type: TextType): keyof JSX.IntrinsicElements {
+function getType(type: TextType): string {
   switch (type) {
     case 'heading1':
       return 'h1';
