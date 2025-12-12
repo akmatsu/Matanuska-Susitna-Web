1ff7f2e126fe104914675076595183926193d006
f3ve <free.eve@pm.me>
2025-12-12 10:20:43 -0900

    feat: added policy page

--- sites/msb/app/policies/[slug]/page.tsx
+++ sites/msb/app/policies/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import { BasePage } from '@/components/static/BasePage';
+import { getClientHandler } from '@/utils/apollo/utils';
 import { GenerateMetadataFunction, getPageMeta } from '@/utils/pageHelpers';
 import { gql } from '@msb/js-sdk/gql';
+import { notFound } from 'next/navigation';
 
 const metaQuery = gql(`
     query GetPolicyMeta($slug: String!) {
@@ -17,10 +20,37 @@ export const generateMetadata: GenerateMetadataFunction = async ({
   return getPageMeta('policy', metaQuery, slug);
 };
 
-// const PolicyQuery = gql(`
-//     query GetPolicy($slug: String!) {
-//         policy(where: { slug: $slug }) {
-//           ...BasePageInfo
-//         }
-//     }
-//   `);
+const PolicyQuery = gql(`
+    query GetPolicy($slug: String!, $now: DateTime!) {
+        policy(where: { slug: $slug }) {
+          ...BasePageInfo
+        }
+    }
+  `);
+
+export default async function PolicyPage(props: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await props.params;
+
+  const { data, error } = await getClientHandler({
+    query: PolicyQuery,
+    variables: {
+      slug,
+      now: new Date().toISOString(),
+    },
+  });
+
+  if (error) {
+    console.error('Error fetching policy page data:', error);
+    throw error;
+  }
+
+  if (!data?.policy) {
+    return notFound();
+  }
+
+  const page = data.policy;
+
+  return <BasePage data={page} />;
+}
