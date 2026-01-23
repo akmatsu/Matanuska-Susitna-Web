faf7688c81e922718eea6d6996c5f4a5a3f2ecc5
f3ve <free.eve@pm.me>
2026-01-23 10:15:46 -0900

    feat: moved oma/parli training video link into new training & resources block

--- sites/msb/components/static/Page/BoardDocuments.tsx
+++ sites/msb/components/static/Page/BoardDocuments.tsx
@@ -1,5 +1,6 @@
 import { FragmentType, getFragmentData, gql } from '@msb/js-sdk/gql';
 import { DocumentLink } from '../DocumentLink';
+import { Link } from '../Link';
 
 const boardDocumentListFragment = gql(`
   fragment BoardDocumentList on Document {
@@ -8,13 +9,41 @@ const boardDocumentListFragment = gql(`
   }
 `);
 
+const boardParliTrainingLinkFragment = gql(`
+  fragment BoardParliTrainingLink on ExternalLink {
+    label
+    url {
+      title
+      url
+    }
+  }
+`);
+
 export function BoardDocuments(props: {
   documents?: FragmentType<typeof boardDocumentListFragment>[] | null;
+  parliTrainingLink?: FragmentType<
+    typeof boardParliTrainingLinkFragment
+  > | null;
 }) {
   const docs = getFragmentData(boardDocumentListFragment, props.documents);
+  const parliTrainingLink = getFragmentData(
+    boardParliTrainingLinkFragment,
+    props.parliTrainingLink,
+  );
 
   return (
     <ul className="list-disc list-inside">
+      {parliTrainingLink?.url?.url && (
+        <li>
+          <Link
+            href={parliTrainingLink.url?.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {parliTrainingLink.label || parliTrainingLink.url.title}
+          </Link>
+        </li>
+      )}
       {docs?.map((doc) => (
         <li key={doc.id} className="list-disc list-inside">
           <DocumentLink data={doc} />
