c30a472be0a183c9406571259092931bbd7a7502
f3ve <free.eve@pm.me>
2026-01-26 12:59:05 -0900

    fix: fixed list item spacing in prose-wrapper

--- sites/msb/components/static/ProseWrapper.tsx
+++ sites/msb/components/static/ProseWrapper.tsx
@@ -8,10 +8,22 @@ export function ProseWrapper(props: {
   return (
     <div
       className={clsx(
-        {
-          'prose max-w-none prose-table:table-auto prose-table:w-full prose-th:bg-base-lighter prose-th:border prose-th:border-base-darkest prose-th:font-bold prose-th:px-2 prose-td:px-2 prose-td:border prose-td:border-base-darkest prose-table:border prose-table:border-base-darkest prose-a:text-primary prose-blockquote:py-4 prose-blockquote:px-4 prose-li:-my-2 text-ink':
-            !props.noProse,
-        },
+        !props.noProse && [
+          // base styles
+          'prose max-w-none text-ink',
+
+          // Links
+          'prose-a:text-primary',
+
+          // Blockquote styles
+          'prose-blockquote:py-4 prose-blockquote:px-4',
+
+          // table styles
+          'prose-table:table-auto prose-table:w-full prose-th:bg-base-lighter prose-table:border prose-table:border-base-darkest',
+          'prose-th:border prose-th:border-base-darkest prose-th:font-bold prose-th:px-2',
+          'prose-td:px-2 prose-td:border prose-td:border-base-darkest',
+        ],
+
         props.className,
       )}
     >
