f6d498f597715d94a9c7532a52366af94b52dfa2
f3ve <free.eve@pm.me>
2026-04-29 16:24:17 -0800

    fix: fixed full client reload on search

--- sites/myprop/app/not-found.tsx
+++ sites/myprop/app/not-found.tsx
@@ -8,29 +8,35 @@ export default function NotFoundPage() {
   return (
     <main className="space-y-4 text-center">
       <PageTitle title="Page Not Found" />
-      <pre
-        className="mx-auto w-fit max-w-full text-left font-mono text-[clamp(0.5rem,1.6vw,1rem)] leading-none whitespace-pre"
-        aria-hidden="true"
-      >
-        ____________/\\\________/\\\\\\\_______________/\\\____
-        <br />
-        &nbsp;__________/\\\\\______/\\\/////\\\___________/\\\\\____
-        <br />
-        &nbsp;&nbsp;________/\\\/\\\_____/\\\____\//\\\________/\\\/\\\____
-        <br />
-        &nbsp;&nbsp;&nbsp;______/\\\/\/\\\____\/\\\_____\/\\\______/\\\/\/\\\____
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;____/\\\/__\/\\\____\/\\\_____\/\\\____/\\\/__\/\\\____
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__/\\\\\\\\\\\\\\\\_\/\\\_____\/\\\__/\\\\\\\\\\\\\\\\_
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_\///////////\\\//__\//\\\____/\\\__\///////////\\\//__
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________\/\\\_____\///\\\\\\\/_____________\/\\\____
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________\///________\///////_______________\///_____
-        <br />
-      </pre>
+      <figure>
+        <pre
+          className="mx-auto w-fit max-w-full text-left font-mono text-[clamp(0.5rem,1.6vw,1rem)] leading-none whitespace-pre"
+          role="img"
+          aria-label="ASCII 404"
+        >
+          ____________/\\\________/\\\\\\\_______________/\\\____
+          <br />
+          &nbsp;__________/\\\\\______/\\\/////\\\___________/\\\\\____
+          <br />
+          &nbsp;&nbsp;________/\\\/\\\_____/\\\____\//\\\________/\\\/\\\____
+          <br />
+          &nbsp;&nbsp;&nbsp;______/\\\/\/\\\____\/\\\_____\/\\\______/\\\/\/\\\____
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;____/\\\/__\/\\\____\/\\\_____\/\\\____/\\\/__\/\\\____
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__/\\\\\\\\\\\\\\\\_\/\\\_____\/\\\__/\\\\\\\\\\\\\\\\_
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_\///////////\\\//__\//\\\____/\\\__\///////////\\\//__
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________\/\\\_____\///\\\\\\\/_____________\/\\\____
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________\///________\///////_______________\///_____
+          <br />
+        </pre>
+        <figcaption className="sr-only">
+          ASCII art of a 404 error code
+        </figcaption>
+      </figure>
       <p>This page either doesn&apos;t exist or has moved.</p>
 
       <p>
--- sites/myprop/components/SearchField/SearchFieldLoaded.tsx
+++ sites/myprop/components/SearchField/SearchFieldLoaded.tsx
@@ -1,5 +1,6 @@
-import { getSearchParams } from '@/utils/serverHelpers';
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useSearchParams, useRouter } from 'next/navigation';
 import { SearchFieldBody } from './SearchFieldBody';
 
 const searchOptions = [
@@ -8,31 +9,33 @@ const searchOptions = [
   { value: 'subid', label: 'Subdivision ID Search' },
 ];
 
-export async function SearchFieldLoaded() {
-  const searchParams = await getSearchParams();
-
-  async function handleSubmit(formData: FormData) {
-    'use server';
-    const searchType = formData.get('search-type');
-    const query = formData.get('query');
-
-    if (typeof searchType === 'string' && typeof query === 'string') {
-      const params = new URLSearchParams();
-      if (searchType?.length) params.set('mode', searchType);
-      if (query?.length) params.set('query', query);
-
-      const searchUrl = `/search?${params.toString()}`;
-      redirect(searchUrl);
-    } else {
-      console.error('Invalid form data:', { searchType, query });
-    }
-  }
+export function SearchFieldLoaded() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchParamsObj = {
+    type: searchParams.get('type') || undefined,
+    query: searchParams.get('query') || undefined,
+  };
 
   return (
     <SearchFieldBody
-      searchParams={searchParams}
+      searchParams={searchParamsObj}
       searchOptions={searchOptions}
-      handleSubmit={handleSubmit}
+      handleSubmit={async (formData: FormData) => {
+        const searchType = formData.get('search-type');
+        const query = formData.get('query');
+
+        if (typeof searchType === 'string' && typeof query === 'string') {
+          const params = new URLSearchParams();
+          if (searchType?.length) params.set('mode', searchType);
+          if (query?.length) params.set('query', query);
+
+          const searchUrl = `/search?${params.toString()}`;
+          router.push(searchUrl);
+        } else {
+          console.error('Invalid form data:', { searchType, query });
+        }
+      }}
     />
   );
 }
