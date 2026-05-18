fdf5e5adaf0f82577dcd3e91c35ba3b1e91b8eb1
f3ve <free.eve@pm.me>
2026-05-18 10:55:54 -0800

    fix: added placeholder for advanced calculator

---
+++ sites/widgets/app/tax-proposals-calculator/advanced/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Tax Proposals Calculator - Advanced',
+};
+
+export default function TaxProposalsCalculatorAdvanced() {
+  return <p>Advanced calc will go here!</p>;
+}
