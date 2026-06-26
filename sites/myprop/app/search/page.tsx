f26201acc42deb6e5d748a7d59a777d847b7d3f7
f3ve <free.eve@pm.me>
2026-06-26 10:02:49 -0800

    feat: added page metadata and fixed dnr links

--- sites/myprop/app/search/page.tsx
+++ sites/myprop/app/search/page.tsx
@@ -3,6 +3,12 @@ import { Results } from './Results';
 import { ResultsLoading } from './ResultsLoading';
 import { SearchField } from '@/components/SearchField';
 
+export const metadata = {
+  title: 'MyProperty - Search',
+  description:
+    'Search for real property information in the Matanuska-Susitna Borough with myProperty. Access parcel data, ownership details, assessments, and maps.',
+};
+
 export default async function SearchPage(props: PageProps<'/search'>) {
   const searchParams = await props.searchParams;
 
