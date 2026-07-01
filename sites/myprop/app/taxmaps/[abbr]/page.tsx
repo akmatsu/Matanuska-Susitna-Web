6a06ffc4824a9ecbbcaa13ea9509092182b89e36
f3ve <free.eve@pm.me>
2026-07-01 09:57:31 -0800

    fix: fixed taxmap images

--- sites/myprop/app/taxmaps/[abbr]/page.tsx
+++ sites/myprop/app/taxmaps/[abbr]/page.tsx
@@ -17,7 +17,7 @@ export default async function TaxMapDetails(
   return (
     <div className="flex flex-col items-center gap-1 md:grid md:grid-cols-2 md:items-start print:grid print:grid-cols-2 print:items-start">
       <div className="relative aspect-469/431 w-full max-w-100">
-        <Image
+        <img
           src={`https://myproperty.matsugov.us/taxmaps/images/${abbr}index.jpg`}
           alt={`TaxMap index for ${abbr} including insets ${data.map((sub) => sub.map).join(', ')}`}
           fill
