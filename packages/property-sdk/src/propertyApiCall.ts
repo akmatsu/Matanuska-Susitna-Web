b9fe3f2aad140097b210c05da6d0abbdc6e88574
f3ve <free.eve@pm.me>
2026-07-28 11:56:18 -0800

    fix: remove commas and periods from search queries, replace directions and address suffixes with proper appreviations

--- packages/property-sdk/src/propertyApiCall.ts
+++ packages/property-sdk/src/propertyApiCall.ts
@@ -2,6 +2,79 @@
 
 const baseUrl = process.env.API_URL || 'http://localhost:3000/api';
 
+const addressSuffixesAndAbbreviations = [
+  {
+    suffix: 'Street',
+    abbreviation: 'St',
+  },
+  {
+    suffix: 'Avenue',
+    abbreviation: 'Ave',
+  },
+  {
+    suffix: 'Boulevard',
+    abbreviation: 'Blvd',
+  },
+  {
+    suffix: 'Road',
+    abbreviation: 'Rd',
+  },
+  {
+    suffix: 'Lane',
+    abbreviation: 'Ln',
+  },
+  {
+    suffix: 'Drive',
+    abbreviation: 'Dr',
+  },
+  {
+    suffix: 'Court',
+    abbreviation: 'Ct',
+  },
+  {
+    suffix: 'Place',
+    abbreviation: 'Pl',
+  },
+  {
+    suffix: 'Terrace',
+    abbreviation: 'Ter',
+  },
+  {
+    suffix: 'Way',
+    abbreviation: 'Way',
+  },
+];
+
+const directionsAndAbbreviations = [
+  {
+    direction: 'North',
+    abbreviation: 'N',
+  },
+  {
+    direction: 'South',
+    abbreviation: 'S',
+  },
+  {
+    direction: 'East',
+    abbreviation: 'E',
+  },
+  {
+    direction: 'West',
+    abbreviation: 'W',
+  },
+];
+
+// need to create directionRegexLikeThis /(\d+\s)({direction})(\b)/
+const directionRegex = new RegExp(
+  `(\\d+\\s)(${directionsAndAbbreviations.map(({ direction }) => direction).join('|')})(\\b)`,
+  'gi',
+);
+
+const suffixRegex = new RegExp(
+  `( \\w+ )(${addressSuffixesAndAbbreviations.map(({ suffix }) => suffix).join('|')})$`,
+  'gi',
+);
+
 /**
  * A helper function to make API calls to the property API. It constructs the full URL using the base URL and the provided path, and includes the API key in the headers. It also handles query parameters and returns the JSON response.
  */
@@ -18,12 +91,39 @@ export async function propertyApiCall<T = unknown>(
   const url = new URL(`/property${normalizedPath}`, baseUrl);
 
   if (params) {
+    // Wee need to modify the query param
     const searchParams = new URLSearchParams();
     for (const [key, value] of Object.entries(params)) {
       if (Array.isArray(value)) {
         value.forEach((v) => searchParams.append(key, v));
       } else {
-        searchParams.append(key, value);
+        let cleanedQuery = value;
+        if (
+          key === 'query' &&
+          (params['mode'] === 'address' || params['mode'] === 'wild')
+        ) {
+          // Need to replace direction with direction abbreviation
+          cleanedQuery = value
+            .trim()
+            .replace(/[.,]/gi, '')
+            .replace(directionRegex, (_, p1, p2, p3) => {
+              const abbreviation =
+                directionsAndAbbreviations.find(
+                  ({ direction }) => direction === p2,
+                )?.abbreviation ?? p2;
+              return `${p1}${abbreviation}${p3}`;
+            })
+            .replace(suffixRegex, (_, p1, p2) => {
+              const abbreviation =
+                addressSuffixesAndAbbreviations.find(
+                  ({ suffix }) => suffix === p2,
+                )?.abbreviation ?? p2;
+              return `${p1}${abbreviation}`;
+            });
+
+          console.log(cleanedQuery);
+        }
+        searchParams.append(key, cleanedQuery);
       }
     }
     url.search = searchParams.toString();
